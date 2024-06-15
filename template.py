67653f109f8ea0e86f569d6961aef418001748f1
Supriyo chakraborty <supriyochakraborty954@gmail.com>
2024-06-15 19:27:24 +0530

    everything updated

--- template.py
+++ template.py
@@ -1,32 +1,25 @@
 import os
 from pathlib import Path
 
+package_name = "mongodb_connect"
+
 list_of_files = [
-   ".github/workflows/.gitkeep",
+   ".github/workflows/ci.yaml",
    "src/__init__.py",
-   "src/components/__init__.py",
-   "src/components/data_ingestion.py", 
-   "src/components/data_transformation.py",
-   "src/components/model_trainer.py",
-   "src/components/model_evaluation.py",
-   "src/pipeline/__init__.py",
-   "src/pipeline/training_pipeline.py",
-   "src/pipeline/prediction_pipeline.py",
-   "src/utils/__init__.py",
-   "src/utils/utils.py",
-   "src/logger/logging.py",
-   "src/exception/exception.py",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/mongo_crud.py", 
+   "tests/__init__.py",
    "tests/unit/__init__.py",
+   "tests/unit/unit.py",
    "tests/integration/__init__.py",
+   "tests/integration/int.py",
    "init_setup.sh",
-   "requirements.txt",
-   "requirements_dev.txt",
+   "requirements.txt", 
    "setup.py",
    "setup.cfg",
    "pyproject.toml",
    "tox.ini",
-   "experiment/experiments.ipynb"
-
+   "experiments/experiments.ipynb", 
 ]
 
 for filepath in list_of_files:
@@ -34,13 +27,7 @@ for filepath in list_of_files:
     filedir, filename = os.path.split(filepath)
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass # create an empty file
-
-
-
-filepath="a/b/c.txt"
-
