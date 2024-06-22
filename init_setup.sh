871886fba002803da9e3cb4c887f50f98c398022
Supriyo chakraborty <supriyochakraborty954@gmail.com>
2024-06-22 12:49:59 +0530

    latest commit

---
+++ init_setup.sh
@@ -0,0 +1,19 @@
+echo [$(date)]: "START"
+
+
+echo [$(date)]: "creating env with python 3.8 version" 
+
+
+conda create --name env python=3.8 -y
+
+
+
+echo [$(date)]: "activating the environment" 
+
+source activate ./env
+
+echo [$(date)]: "installing the dev requirements" 
+
+pip install -r requirements_dev.txt
+
+echo [$(date)]: "END" 
